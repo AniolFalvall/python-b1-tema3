97dff33a413d00132e2679b8cba9922b6a7d8797
Aniol Falgueras <aniolfv2@gmail.com>
2025-12-12 19:09:17 +0100

    Solution to ej3a2 added

--- 3a/ej3a2/ej3a2_math_package/arithmetic.py
+++ 3a/ej3a2/ej3a2_math_package/arithmetic.py
@@ -2,11 +2,11 @@
 import math
 
 def power(base: float, exponent: float) -> float:
-	# Write your code here
+	return math.pow(base, exponent)
 	pass
 
 def square_root(num_1: float) -> float:
-	# Write your code here
+	return math.sqrt(num_1)
 	pass
 
 
