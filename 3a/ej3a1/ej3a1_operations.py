2689185123148c1d3e68bae30c8008e149cdbe48
Aniol Falgueras <aniolfv2@gmail.com>
2025-12-12 18:56:54 +0100

    Solution to ej3a1 added

--- 3a/ej3a1/ej3a1_operations.py
+++ 3a/ej3a1/ej3a1_operations.py
@@ -2,20 +2,24 @@
 
 
 def add(num_1: float, num_2: float) -> float:
-    # Write here your code
+    sum = num_1 + num_2
+    return sum
     pass
 
 
 def subtract(num_1: float, num_2: float) -> float:
-    # Write here your code
+    substraction = num_1 - num_2
+    return substraction
     pass
 
 
 def multiply(num_1: float, num_2: float) -> float:
-    # Write here your code
+    multiplication = num_1 * num_2
+    return multiplication
     pass
 
 
 def divide(num_1: float, num_2: float) -> float:
-    # Write here your code
+    division = num_1 / num_2
+    return division
     pass
